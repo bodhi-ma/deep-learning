5dd4d17cf4c8db91376e9dc63ccfd52e1627c920
maguozhen <839363837@qq.com>
2019-12-26 17:11:49 +0800

    typo

--- src/ch04/gradient_simplenet.py
+++ src/ch04/gradient_simplenet.py
@@ -1,14 +1,12 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 为了导入父目录中的文件而进行的设定
 import numpy as np
-from common.functions import softmax, cross_entropy_error
-from common.gradient import numerical_gradient
+from src.common.functions import softmax, cross_entropy_error
+from src.common.gradient import numerical_gradient
 
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.randn(2,3)
+        self.W = np.random.randn(2, 3)
 
     def predict(self, x):
         return np.dot(x, self.W)
@@ -19,6 +17,7 @@ class simpleNet:
         loss = cross_entropy_error(y, t)
 
         return loss
+
 
 x = np.array([0.6, 0.9])
 t = np.array([0, 0, 1])
